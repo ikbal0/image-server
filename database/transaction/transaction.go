4692fad6486707adbb1939a91b077c1c70979653
Ikbal Yaduar Taupik <Ikbal.ytaufik@gmail.com>
2023-05-21 13:46:09 +0700

    fix

--- database/transaction/transaction.go
+++ database/transaction/transaction.go
@@ -1,14 +1,1 @@
 package transaction
-
-import (
-	"gorm.io/gorm"
-)
-
-type TransactionImpl struct {
-	db *gorm.DB
-}
-
-func NewTransaction(db *gorm.DB) *TransactionImpl {
-	tx := &TransactionImpl{db: db}
-	return tx
-}
