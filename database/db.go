f07b0439434f91321f88a3408dc04b6d53968715
Ikbal Yaduar Taupik <Ikbal.ytaufik@gmail.com>
2023-07-10 09:48:47 +0700

    adding env caller

--- database/db.go
+++ database/db.go
@@ -2,41 +2,43 @@ package database
 
 import (
 	"fmt"
+	"image-server/src/modules/image/entities"
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-// var (
-// 	host     = os.Getenv("PGHOST")
-// 	port     = os.Getenv("PGPORT")
-// 	user     = os.Getenv("PGUSER")
-// 	password = os.Getenv("PGPASSWORD")
-// 	dbname   = os.Getenv("PGDATABASE")
-// 	db       *gorm.DB
-// 	err      error
-// )
-
 var (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "postgresql"
-	dbname   = "image-server-go"
+	host     = os.Getenv("PGHOST")
+	port     = os.Getenv("PGPORT")
+	user     = os.Getenv("PGUSER")
+	password = os.Getenv("PGPASSWORD")
+	dbname   = os.Getenv("PGDATABASE")
 	db       *gorm.DB
 	err      error
 )
 
+// var (
+// 	host     = "localhost"
+// 	port     = 5432
+// 	user     = "postgres"
+// 	password = "postgresql"
+// 	dbname   = "image-server-go"
+// 	db       *gorm.DB
+// 	err      error
+// )
+
 func StartDB() {
-	config := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode= disable", host, user, password, dbname, port)
+	config := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode= disable", host, user, password, dbname, port)
 	db, err = gorm.Open(postgres.Open(config), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal("error connecting to database:", err)
 	}
 
-	// db.Debug().AutoMigrate(models.Image{})
+	db.Debug().AutoMigrate(entities.Image{})
 }
 
 func GetDB() *gorm.DB {
